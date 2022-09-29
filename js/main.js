ed7408784a695ce204003e9781ccb43bc676f271
simon <=sariasneira@gmail.com>
2022-09-28 23:38:26 -0300

    Incorporando librerias

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
+// Hello.
+//
+// This is JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+//
+// To start, simply enter some JavaScript anywhere on this page. Your
+// report will appear on the right side.
+//
+// Additionally, you can toggle specific options in the Configure
+// menu.
+
 // @ts-check
 //Simulador de becas universitarias
 function ponderacion(resultado,puntajes){
     let total = 0;
     for(let i = 0; i < resultado.ponderacion.length ;i++){
-        total += resultado.ponderacion[i] * puntajes[i]
+        total += resultado.ponderacion[i] * puntajes[i];
     }
     return total;
-};
+}
 
 function beca(resultado,puntaje){
     for(let i = 0; i<resultado.beca_puntaje.length; i++){
@@ -15,18 +26,49 @@ function beca(resultado,puntaje){
         }
     }
     return 0;
-};
+}
 
 /*Output condicional en base a los puntajes ingresados */
 function mostrarResultado(carrera,lengua,mates,ciencia,nem,ranking){
     let resultado = datos_carreras.find((el)=> el.carrera_id === carrera);
     let ponderado = ponderacion(resultado,[nem,ranking,mates,lengua,ciencia]);
-    let beca_simulada = beca(resultado,ponderado);
-    resultadoDOM.innerHTML = `<p>Su puntaje ponderado es de: ${ponderado}</p><p>Su beca simulada cubre el ${beca_simulada}% de su carrera de <strong>${resultado.carrera}</strong></p>` ;
+    let becaSimulada = beca(resultado,ponderado);
+    resultadoDOM.innerHTML = `<p>Su puntaje ponderado es de: ${ponderado}</p><p>Su beca simulada cubre el ${becaSimulada}% de su carrera de <strong>${resultado.carrera}</strong></p>` ;
     
     /*Operador optimizado*/
-    beca_simulada> 0 ? felicitacionDOM.innerHTML = "<h2>FELICITACIONES</h2><p>Tu puntaje simulado te hace elegible  para una beca, para más información visita la secciones de \"Financiamiento\"</p>" : felicitacionDOM.innerHTML = "";
-};
+    becaSimulada> 0 ? felicitacionDOM.innerHTML = "<h2>FELICITACIONES</h2><p>Tu puntaje simulado te hace elegible  para una beca, para más información visita la secciones de \"Financiamiento\"</p>" : felicitacionDOM.innerHTML = "";
+}
+
+/* Cuadro que verifica la identidad del usuario, solo existe el usuario admin con contraseña 12345 */
+function cuadroLogin(){
+    Swal.fire({
+        title: 'Inicio de sesión',
+        html:
+        '<input id="usuario"    class="swal2-input" type="text" placeholder="Usuario">' +
+        '<input id="contrasena" class="swal2-input" type="password" placeholder="Contraseña">',
+        focusConfirm: false,
+        preConfirm: () => {
+            const formValues =  [document.getElementById('usuario').value, document.getElementById('contrasena').value];
+              
+            if(formValues[0] == "admin" && formValues[1] == "12345"){
+                Swal.fire({
+                title: 'Bienvenido',
+                icon: 'success',
+                confirmButtonText: 'Aceptar'
+                });
+                sessionStorage.setItem("usuario",formValues[0]);
+                inicioSesion.innerHTML = `<a href="#">${sessionStorage.getItem("usuario")}</a>`;
+            } else{
+                Swal.fire({
+                title: 'Error',
+                text: 'credenciales no validas',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+                });
+            }
+        }
+    });
+}
 
 /*Datos estandar para el calculo del puntaje, son constantes */
 let datos_carreras =[
@@ -35,24 +77,10 @@ let datos_carreras =[
     {carrera_id:3, carrera: "Ingeniería", ponderacion: [.1, .25, .2,  .1,  .2 ], beca_puntaje:[750, 700, 650], beca_porcentaje:[90,  85, 75]}
 ];
 
-/*Tarjeta de Log In como Pop Up en la pagina */
-let tarjetaLogIn = document.createElement("div");
-tarjetaLogIn.classList.add('enterCard');
-tarjetaLogIn.innerHTML = "<div><p>Inicio de sesión</p></div><div><form id='loginCard'><input type='text' placeholder='usuario'><input type='submit' value='Aceptar'></form></div>";
-
 /* Utilización del session storage para almacenar examnes y/o ensayos anteriores realizados por la persona*/
 let inicioSesion = document.getElementById("loginBtn");
-inicioSesion.onclick = () => {document.body.append(tarjetaLogIn);
-                              let formularioSesion = document.getElementById("loginCard");
-                              formularioSesion.addEventListener("submit",(e)=>{
-                                e.preventDefault();
-                                let formulario = e.target;
-                                sessionStorage.setItem("usuario",formulario.children[0].value);
-                                inicioSesion.innerHTML = `<a href="#">${sessionStorage.getItem("usuario")}</a>`;
-                                let tarjeta = document.querySelector(".enterCard");
-                                tarjeta.remove();
-                              })
-};
+inicioSesion.onclick = cuadroLogin;
+
 
 /* Simulación del puntaje ponderado en base al formulario */
 let resultadoDOM = document.getElementById("resultadoSimulacion");
